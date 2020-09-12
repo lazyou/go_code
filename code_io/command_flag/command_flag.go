733f66ce6782e78b1e307c2c1f68cfbdc2472a8e
lazyou <1394139539@qq.com>
2020-09-12 10:33:03 +0800

    命令行参数: 注释 flag 相关方法解读

--- code_io/command_flag/command_flag.go
+++ code_io/command_flag/command_flag.go
@@ -19,9 +19,11 @@ func main() {
 	// 默认打印选项提示
 	flag.PrintDefaults()
 
+	// 扫描参数列表（或者常量列表）并设置 flag
 	flag.Parse() // Scans the arg list and sets up flags
 
 	var s string = ""
+	// flag.Narg() 返回参数的数量
 	for i := 0; i < flag.NArg(); i++ {
 		if i > 0 {
 			s += Space
@@ -29,6 +31,9 @@ func main() {
 				s += Newline
 			}
 		}
+
+		// flag.Arg(i) 表示第i个参数
+		// flag.Arg(0) 就是第一个真实的 flag，而不是像 os.Args(0) 放置程序的名字
 		s += flag.Arg(i)
 	}
 
